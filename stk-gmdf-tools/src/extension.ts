de09dc9914980c348517b99359d3a5cf6b67b20d
Ed Mackey <elm19087@gmail.com>
2018-09-18 11:11:38 -0400

    Getting things ready for extract/inject commands.

--- stk-gmdf-tools/src/extension.ts
+++ stk-gmdf-tools/src/extension.ts
@@ -1,27 +1,18 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "stk-gmdf-tools" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
+    context.subscriptions.push(vscode.commands.registerCommand('gmdf.extractFromGltf', () => {
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World!  From GMDF.');
-    });
+    }));
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('gmdf.injectIntoGltf', () => {
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World!  From GMDF.');
+    }));
 }
 
 // this method is called when your extension is deactivated
